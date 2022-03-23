0f75ec2464d28129c10fd991191018d66917798d
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2022-03-23 10:02:27 +0100

    doc

--- src/models/User.js
+++ src/models/User.js
@@ -18,6 +18,13 @@ export const findOneUtilisateurByEmailPSD = (profil, cb) => {
     });
 }
 
+/**
+ * TODO : a revoir
+ * Méthode permettant de mettre à jour les informations d'un utilisateur
+ * @param updateString {string} chaine de caractère contenant les informations à mettre à jour
+ * @param id {int} id de l'utilisateur
+ * @param cb {callback} traitement du résultat
+ */
 export const updateUtilisateur = (updateString, id, cb) => {
     db.query('UPDATE utilisateur SET ? WHERE idUtilisateur = ? ;', [updateString, id], function (err, data) {
         if (err) {
@@ -32,13 +39,22 @@ export const updateUtilisateur = (updateString, id, cb) => {
     });
 }
 
+/**
+ * Méthode permettant de mettre à jour le token d'un utilisateur
+ * @param email {string} email de l'utilisateur
+ * @param token {string} token à mettre à jour
+ * @param tokenTimeStamp {string} timestamp du token
+ */
 export const updateUserToken = (email, token, tokenTimeStamp) => {
     db.query('UPDATE utilisateur SET token = ?, tokenTimeStamp = ? WHERE Email = ?;', [token, tokenTimeStamp, email], function (err, data) {
         if (err) throw err;
     });
 }
 
-// TODO : Doc
+/**
+ * Méthode permettant de créer un utilisateur en base de données
+ * @param donnees {array<string>} données à insérer
+ */
 export const createUser = (donnees) => {
     db.query('INSERT INTO utilisateur (Nom, Prenom, Email, MotDePasse, PhotoProfile, Type) VALUES (?,?,?,?,"default","user");', donnees, function (err, data) {
         if (err) throw err;
