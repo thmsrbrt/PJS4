4751a8dde612cbe1fc3eb0c5cde1c654f8414141
thmsrbrt <30445476+thmsrbrt@users.noreply.github.com>
2022-03-30 16:44:24 +0200

    données BDD

--- ressources/BDD.sql
+++ ressources/BDD.sql
@@ -261,7 +261,7 @@ VALUES (1, 'Thomas', 'Robert', 'thomas.robert@icoud.com', 'bucUJKdRehLFuAXB9JMXI
         '1643362441723.jpeg', 'A la recherche dun stage en Cybersécurité ', 'pdf_modèle.pdf', 'Candidat',
         '2022-01-01 09:58:44'),                                             -- é/RTZJN(iuzdj4knfqd;.a@hea789er-ae
        (7, 'Fabien', 'Rondan', 'fabien.rondan@protonmail.com', 'uclQZA4bN0DpisuT5mnGV2b2Zw3RYJupH/QQUrpIxvM=',
-        '1638893705524.jpeg', 'Acctuelement en DUT Informatique à lIUT Paris Rives de Seine', 'pdf_modèle.pdf', 'Candidat',
+        '1638893705524.jpeg', 'Actuellement en DUT Informatique à lIUT Paris Rives de Seine', 'pdf_modèle.pdf', 'Candidat',
         '2022-01-01 09:58:44'),                                             -- password1234
        (8, 'Fabien', 'Rondan', 'fabiendu56334@icoud.c', 'UO7328CJnIfAi+39/m+zk1pjirJKkfJ2VRdDJXSme/g=',
         '1638893705524.jpeg', 'A la recherche dun jobs dété', 'pdf_modèle.pdf', 'Candidat',
@@ -331,7 +331,7 @@ VALUES (1, 1, 3, 'friends', null),
 
 INSERT INTO Message(idMessage, Message, DateEnvoi, idUtilisateur, idConversation)
 VALUES (12343, 'Bonjour, je suis intéressé ...', '2022-01-24 10:21:20', 1, 1),
-       (12345, 'Bonjour, votre candidature nous plait beaucoup, etes vous dispo dem pour un test ',
+       (12345, 'Bonjour, votre candidature nous plait beaucoup, êtes vous disponible demain pour un test ',
         '2022-01-24 11:43:20', 3, 1),
        (12346, 'message pour la conv 2 ', '2022-01-24 11:43:20', 3, 2),
        (34343, 'Je suis disponible demain 1O heure pour passer le teste ', '2022-01-24 14:21:20', 1, 1),
@@ -339,24 +339,66 @@ VALUES (12343, 'Bonjour, je suis intéressé ...', '2022-01-24 10:21:20', 1, 1),
        (1, 'Bonjour, je suis intéressé par votre entreprise, et le poste que vous proposez .', '2022-01-24 9:21:20', 5,
         3),
        (53432,
-        'Bonjour nom, le poste a déja été pris, bonne continuation pour vos recherche noublié pas de regarder nos offre sur le site : www.offre-de-merde.fr',
+        'Bonjour nom, le poste a déja été pris, bonne continuation pour vos recherche noublié pas de regarder nos offre sur le site : www.offre-de-stage.fr',
         '2022-01-25 9:21:20', 11, 3),
-       (642432, 'Bonjour Francois, \n je suis très intéressé par votre offre blabla ', '2022-01-24 10:00:00', 7, 111),
+       (642432, 'Bonjour Francois, \n je suis très intéressé par votre offre Bonne continuation', '2022-01-24 10:00:00', 7, 111),
        (6764564, 'Bonjour clement, le poste est toujours a pourvoir blabla', '2022-01-26 14:59:20', 18, 111),
-       (7846543, 'Je peux faire lentretient cette semaine blbal ', '2022-01-30 16:21:58', 7, 111),
+       (7846543, 'Je peux faire l entretient cette semaine si ca va de votre coté ?', '2022-01-30 16:21:58', 7, 111),
        (24632323,
-        'Oui pas de probleme, je peux vous proposez par Teams Vendredi 15 heure ? \n dans l\attente de votre retour. blabla',
+        'Oui pas de problème, je peux vous proposez par Teams Vendredi 15 heure ? \n dans l attente de votre retour. blabla',
         '2022-01-24 14:21:20', 18, 111),
-       (4, 'Bonjour, je suis intéressé par cette offre qui me correspond à 100% blzblz', '2022-01-24 15:21:20', 4, 10);
+       (4, 'Bonjour, je suis intéressé par cette offre qui me correspond à 100% au vu de mon parcourt ', '2022-01-24 15:21:20', 4, 10);
 
 
 INSERT INTO Candidature(cvfile, lettremotivation, idannonce, idcandidat)
-VALUES ('CV_ROBERT_Thomas_2022-alt.pdf', 'blabalbalblablal balbla ', 1, 2),
-       ('CV_ROBERT_Thomas_2022-alt.pdf', 'blabalbalblablal balbla ', 1, 3),
-       ('CV_ROBERT_Thomas_2022-alt.pdf', 'blabalbalblablal balbla ', 12, 2),
-       ('CV_ROBERT_Thomas_2022-alt.pdf', 'blabalbalblablal balbla ', 1, 4),
-       ('CV_ROBERT_Thomas_2022-alt.pdf', 'blabalbalblablal balbla ', 3, 7),
-       ('CV_ROBERT_Thomas_2022-alt.pdf', 'blabalbalblablal balbla ', 12, 4);
+VALUES ('CV_ROBERT_Thomas_2022-alt.pdf', 'Objet : Candidature au poste de (emploi)
+(Madame, Monsieur),
+Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).
+En effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m''a permis d''acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel
+(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie.
+Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.
+Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance
+Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations.', 1, 2),
+       ('CV_ROBERT_Thomas_2022-alt.pdf', 'Objet : Candidature au poste de (emploi)
+(Madame, Monsieur),
+Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).
+En effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m''a permis d''acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel
+(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie.
+Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.
+Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance
+Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations. ', 1, 3),
+       ('CV_ROBERT_Thomas_2022-alt.pdf', 'Objet : Candidature au poste de (emploi)
+(Madame, Monsieur),
+Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).
+En effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m''a permis d''acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel
+(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie.
+Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.
+Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance
+Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations. ', 12, 2),
+       ('CV_ROBERT_Thomas_2022-alt.pdf', 'Objet : Candidature au poste de (emploi)
+(Madame, Monsieur),
+Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).
+En effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m''a permis d''acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel
+(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie.
+Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.
+Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance
+Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations. ', 1, 4),
+       ('CV_ROBERT_Thomas_2022-alt.pdf', 'Objet : Candidature au poste de (emploi)
+(Madame, Monsieur),
+Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).
+En effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m''a permis d''acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel
+(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie.
+Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.
+Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance
+Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations. ', 3, 7),
+       ('CV_ROBERT_Thomas_2022-alt.pdf', 'Objet : Candidature au poste de (emploi)
+(Madame, Monsieur),
+Etant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).
+En effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m''a permis d''acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel
+(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie.
+Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.
+Restant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance
+Veuillez agréer, (Madame, Monsieur), l’expression de mes sincères salutations. ', 12, 4);
 
 
 INSERT INTO `userfav` (`idFav`, `idUser`, `idAnnonce`) VALUES
